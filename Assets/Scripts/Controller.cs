84ff718fd3adf2a4775fa49118879e33975c2823
mitcon <koizumi741@gmail.com>
2019-05-04 22:56:40 +0900

    first commit

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour
+{
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.X)) {
+            SceneManager.LoadScene("SampleScene" ,LoadSceneMode.Single);
+        }
+        if(Input.GetKeyDown(KeyCode.Z)) {
+            Instantiate(player);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        float y = Input.GetAxis("Jump");
+
+        Vector3 movement = new Vector3(x,y,z);
+
+        if(rb.position.y < 0){
+            SceneManager.LoadScene("SampleScene",LoadSceneMode.Single);
+        }
+        rb.AddForce(Vector3.Normalize(movement) * speed);
+
+
+    }
+}
